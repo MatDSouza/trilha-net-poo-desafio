64cee02a88bfe72f89e7df48782e8f845c04151e
Mateus de Souza <masori.galo9@hotmail.com>
2024-04-06 12:08:16 -0300

    Desafio completo de Poo

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,29 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
+        private int memoria;
+
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo{ get; set; }
+        private string Imei{ get; set; }
+        private string Memoria{ get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, string memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+        }
+
+        protected Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            this.memoria = memoria;
         }
 
         public void Ligar()
